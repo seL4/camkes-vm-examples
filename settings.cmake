eca5fa5d9acce5ffc9cd746b80ebcdb7dc094054
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-10-03 14:12:02 +1000

    CMake: Added settings.cmake
    Added settings.cmake file to import camkes_vm_settings.cmake.

---
+++ settings.cmake
@@ -0,0 +1,20 @@
+#
+# Copyright 2018, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+cmake_minimum_required(VERSION 3.8.2)
+
+find_file(VM_SETTINGS_PATH camkes_vm_settings.cmake PATHS ${CMAKE_SOURCE_DIR}/projects/vm/ CMAKE_FIND_ROOT_PATH_BOTH)
+mark_as_advanced(FORCE VM_SETTINGS_PATH)
+if("${VM_SETTINGS_PATH}" STREQUAL "VM_SETTINGS_PATH-NOTFOUND")
+    message(FATAL_ERROR "Failed to find camkes_vm_settings.cmake. Consider cmake -DVM_SETTINGS_PATH=/path/to/camkes_vm_settings.cmake")
+endif()
+include(${VM_SETTINGS_PATH})
