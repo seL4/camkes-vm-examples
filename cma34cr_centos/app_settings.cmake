211d48ee65b153481693635a49dcaf6caafc2897
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2018-10-23 17:35:00 +1100

    CMake: Refactored example apps cmake settings
    Moved cmake settings that existed within each apps CMakeLists into
    their own app_settings.cmake file.

---
+++ cma34cr_centos/app_settings.cmake
@@ -0,0 +1,34 @@
+#
+# Copyright 2018, Data61
+# Commonwealth Scientific and Industrial Research Organisation (CSIRO)
+# ABN 41 687 119 230.
+#
+# This software may be distributed and modified according to the terms of
+# the BSD 2-Clause license. Note that NO WARRANTY is provided.
+# See "LICENSE_BSD2.txt" for details.
+#
+# @TAG(DATA61_BSD)
+#
+
+cmake_minimum_required(VERSION 3.8.2)
+
+# Define kernel config options
+set(KernelX86Sel4Arch x86_64 CACHE STRING "" FORCE)
+set(KernelMaxNumNodes 2 CACHE STRING "" FORCE)
+set(KernelMultibootGFXMode linear CACHE STRING "" FORCE)
+set(KernelMultibootGFXModeWidth 0 CACHE STRING "" FORCE)
+set(KernelMultibootGFXModeDepth 0 CACHE STRING "" FORCE)
+set(KernelMultibootGFXModeHeight 0 CACHE STRING "" FORCE)
+
+# cma34cr platform doesn't support HugePage or PCID
+set(KernelHugePage OFF CACHE BOOL "" FORCE)
+set(KernelSupportPCID OFF CACHE BOOL "" FORCE)
+
+# Ignore EPT violations
+set(LibSel4VMMIgnoreEPTViolation ON CACHE BOOL "" FORCE)
+
+# Define VMM configurations
+set(LibSel4VMMVESAFrameBuffer ON CACHE BOOL "" FORCE)
+
+# Enable LWIP
+set(LibLwip ON CACHE BOOL "" FORCE)
