2c0ae6609e4c2f36324e7880490b54dcc946fe78
Adam Felizzi <Adam.Felizzi@data61.csiro.au>
2019-03-13 17:27:24 +1100

    Virtio Net: Use muslc arp header defintions
    Refactored the use the arp header struct to use the defintion
    provided in muslc. This removes duplication and unnecessary
    definitions.

--- components/VM/src/modules/virtio_net/virtio_net_arping.c
+++ components/VM/src/modules/virtio_net/virtio_net_arping.c
@@ -20,59 +20,44 @@
 
 #include "virtio_net_arping.h"
 
-#define PROTO_ARP 0x0806
-#define MAC_LENGTH 6
 #define IPV4_LENGTH 4
 #define ARP_REQUEST 0x01
 #define ARP_REPLY 0x02
 #define HW_TYPE 1
 
-struct arp_header {
-    unsigned short hardware_type;
-    unsigned short protocol_type;
-    unsigned char hardware_len;
-    unsigned char protocol_len;
-    unsigned short opcode;
-    unsigned char sender_mac[MAC_LENGTH];
-    unsigned char sender_ip[IPV4_LENGTH];
-    unsigned char target_mac[MAC_LENGTH];
-    unsigned char target_ip[IPV4_LENGTH];
-};
-
 void arping_reply(char *eth_buffer, virtio_net_t *virtio_net) {
 
-    void *buffer = (void *)eth_buffer;
-    if(buffer == NULL) {
+    if(eth_buffer == NULL) {
         return;
     }
-    struct ethhdr *rcv_req = (struct ethhdr *) buffer;
-    struct arp_header *arp_req = (struct arp_header *) (buffer + sizeof(struct ethhdr));
-    if (ntohs(rcv_req->h_proto) == PROTO_ARP) {
+    struct ethhdr *rcv_req = (struct ethhdr *) eth_buffer;
+    struct ether_arp *arp_req = (struct ether_arp *) (eth_buffer + sizeof(struct ethhdr));
+    if (ntohs(rcv_req->h_proto) == ETH_P_ARP) {
         unsigned char reply_buffer[1500];
         struct ethhdr *send_reply = (struct ethhdr *) reply_buffer;
-        struct arp_header *arp_reply = (struct arp_header *) (reply_buffer + sizeof(struct ethhdr));
+        struct ether_arp *arp_reply = (struct ether_arp *) (reply_buffer + sizeof(struct ethhdr));
 
-        memcpy(send_reply->h_dest, arp_req->sender_mac, MAC_LENGTH);
+        memcpy(send_reply->h_dest, arp_req->arp_sha, ETH_ALEN);
         send_reply->h_proto = htons(ETH_P_ARP);
 
         // MAC Address
-        memcpy(arp_reply->target_mac, arp_req->sender_mac, MAC_LENGTH);
-        memcpy(arp_reply->sender_mac, arp_req->sender_mac, MAC_LENGTH);
-        arp_reply->sender_mac[5] = arp_reply->sender_mac[5] + 2;
+        memcpy(arp_reply->arp_tha, arp_req->arp_sha, ETH_ALEN);
+        memcpy(arp_reply->arp_sha, arp_req->arp_sha, ETH_ALEN);
+        arp_reply->arp_sha[5] = arp_reply->arp_sha[5] + 2;
 
-        memcpy(send_reply->h_source, arp_reply->sender_mac, MAC_LENGTH);
+        memcpy(send_reply->h_source, arp_reply->arp_sha, ETH_ALEN);
         // IP Addresss
-        memcpy(arp_reply->sender_ip, (void *)arp_req->target_ip, IPV4_LENGTH);
-        memcpy(arp_reply->target_ip, (void *)arp_req->sender_ip, IPV4_LENGTH);
+        memcpy(arp_reply->arp_spa, (void *)arp_req->arp_tpa, IPV4_LENGTH);
+        memcpy(arp_reply->arp_tpa, (void *)arp_req->arp_spa, IPV4_LENGTH);
         // Misc arp fields
-        arp_reply->hardware_type = htons(HW_TYPE);
-        arp_reply->protocol_type = htons(ETH_P_IP);
-        arp_reply->opcode = htons(ARP_REPLY);
-        arp_reply->hardware_len = MAC_LENGTH;
-        arp_reply->protocol_len = IPV4_LENGTH;
+        arp_reply->ea_hdr.ar_hrd = htons(HW_TYPE);
+        arp_reply->ea_hdr.ar_pro = htons(ETH_P_IP);
+        arp_reply->ea_hdr.ar_op = htons(ARP_REPLY);
+        arp_reply->ea_hdr.ar_hln = ETH_ALEN;
+        arp_reply->ea_hdr.ar_pln = IPV4_LENGTH;
 
         unsigned int len[1];
-        len[0] = 42;
+        len[0] = sizeof(struct ethhdr) + sizeof(struct ether_arp);
         void *cookie;
         void *emul_buf = (void*)virtio_net->emul_driver->i_cb.allocate_rx_buf(virtio_net->emul_driver->cb_cookie, len[0], &cookie);
         if (emul_buf) {
