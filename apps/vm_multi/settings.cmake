d5aec4d3f19f4d79e9ef0471c0d6e3b18aeb852a
Alison Felizzi <Alison.Felizzi@data61.csiro.au>
2020-02-26 15:50:19 +1100

    vm_multi: Support for qemu-arm-virt
    Updated the vm_multi app such that it supports the qemu-arm-virt
    platform.

--- apps/vm_multi/settings.cmake
+++ apps/vm_multi/settings.cmake
@@ -10,7 +10,7 @@
 # @TAG(DATA61_BSD)
 #
 
-set(supported "exynos5422")
+set(supported "exynos5422;qemu-arm-virt")
 if(NOT "${PLATFORM}" IN_LIST supported)
     message(FATAL_ERROR "PLATFORM: ${PLATFORM} not supported.
          Supported: ${supported}")
@@ -21,3 +21,10 @@ set(VmVirtioConsole ON CACHE BOOL "" FORCE)
 set(VmVirtioNetArping OFF CACHE BOOL "" FORCE)
 set(VmVirtioNetVirtqueue ON CACHE BOOL "" FORCE)
 set(VmInitRdFile ON CACHE BOOL "" FORCE)
+if("${PLATFORM}" STREQUAL "qemu-arm-virt")
+    set(QEMU_MEMORY "2048")
+    set(KernelArmCPU cortex-a53 CACHE STRING "" FORCE)
+    set(qemu_sim_extra_args "-netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device virtio-net,netdev=mynet0,mac=52:55:00:d1:55:01")
+    set(KernelArmExportPCNTUser ON CACHE BOOL "" FORCE)
+    set(KernelArmExportPTMRUser ON CACHE BOOL "" FORCE)
+endif()
