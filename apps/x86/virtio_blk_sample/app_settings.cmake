037032fe9939c3cbdbc3d4615b85189d6bf1fbc2
Damon Lee <Damon.Lee@data61.csiro.au>
2021-04-09 16:10:47 +1000

    x86: Add new virtio_blk_sample test
    This new application is used as an example to showcase the VirtIO block
    driver. This application is based off of Dornerworks' test application
    in this commit: 3f80ae7d on Dornerworks' fork of camkes-vm-examples on
    GitHub.
    
    Signed-off-by: Damon Lee <Damon.Lee@data61.csiro.au>

---
+++ apps/x86/virtio_blk_sample/app_settings.cmake
@@ -0,0 +1,21 @@
+#
+# Copyright 2019, DornerWorks
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+cmake_minimum_required(VERSION 3.8.2)
+
+# Define kernel config options
+set(KernelSel4Arch ia32 CACHE STRING "")
+
+set(KernelMaxNumNodes 1 CACHE STRING "" FORCE)
+set(KernelHugePage OFF CACHE BOOL "" FORCE)
+
+ApplyCommonSimulationSettings(${KernelArch})
+set(KernelIOMMU ON CACHE BOOL "" FORCE)
+
+# Use AHCI by default
+set(SataserverUseAHCI ON CACHE STRING "")
+
+set(CAmkESVMDestHardware "optiplex" CACHE STRING "" FORCE)
