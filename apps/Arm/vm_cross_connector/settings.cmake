0bd006eadb79a6b38091f55eb527ccd01fe1dcbf
Jouni Raunio <jouni.raunio@unikie.com>
2021-09-29 16:19:57 +0300

    HYPR-214: committed basic files.

--- apps/Arm/vm_cross_connector/settings.cmake
+++ apps/Arm/vm_cross_connector/settings.cmake
@@ -1,5 +1,5 @@
 #
-# Copyright 2018, Data61, CSIRO (ABN 41 687 119 230)
+# Copyright 2019, Data61, CSIRO (ABN 41 687 119 230)
 #
 # SPDX-License-Identifier: BSD-2-Clause
 #
@@ -9,16 +9,12 @@ if(NOT "${PLATFORM}" IN_LIST supported)
     message(FATAL_ERROR "PLATFORM: ${PLATFORM} not supported.
          Supported: ${supported}")
 endif()
-set(LibUSB OFF CACHE BOOL "" FORCE)
 set(VmPCISupport ON CACHE BOOL "" FORCE)
-set(VmVirtioConsole ON CACHE BOOL "" FORCE)
-set(VmVirtioNetArping OFF CACHE BOOL "" FORCE)
-set(VmVirtioNetVirtqueue ON CACHE BOOL "" FORCE)
+set(LibUSB OFF CACHE BOOL "" FORCE)
 set(VmInitRdFile ON CACHE BOOL "" FORCE)
-if("${PLATFORM}" STREQUAL "qemu-arm-virt")
+set(VmVirtioConsole ON CACHE BOOL "" FORCE)
+if(${PLATFORM} STREQUAL "qemu-arm-virt")
+     # force cpu
     set(QEMU_MEMORY "2048")
     set(KernelArmCPU cortex-a53 CACHE STRING "" FORCE)
-    set(qemu_sim_extra_args "-netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no -device virtio-net,netdev=mynet0,mac=52:55:00:d1:55:01")
-    set(KernelArmExportPCNTUser ON CACHE BOOL "" FORCE)
-    set(KernelArmExportPTMRUser ON CACHE BOOL "" FORCE)
 endif()
