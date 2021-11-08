9435da2b235d817cbdfe2f1bfdf12f76c76d0a0b
Siwei Zhuang <siwei.zhuang@unsw.edu.au>
2021-11-09 10:57:01 +1100

    skylake: Add new VM
    Signed-off-by: Siwei Zhuang <siwei.zhuang@unsw.edu.au>

---
+++ apps/x86/skylake/app_settings.cmake
@@ -0,0 +1,11 @@
+#
+# Copyright 2018, Data61, CSIRO (ABN 41 687 119 230)
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+cmake_minimum_required(VERSION 3.8.2)
+
+# Define kernel config options
+set(KernelSel4Arch x86_64 CACHE STRING "" FORCE)
+set(KernelMaxNumNodes 1 CACHE STRING "" FORCE)
