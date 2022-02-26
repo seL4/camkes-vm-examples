5757fd6985923caee91db373682bb4dd83c6bd98
Siwei Zhuang <siwei.zhuang@unsw.edu.au>
2022-02-26 23:47:05 +1100

    vm_cross_connector: add second VM
    Added another VM, both connecting to the cross vm component. UART is
    passed through to vm0, so only vm0 can print. Cross vm component
    receives event from vm0 and vm1. Tested on qemu.
    
    Signed-off-by: Siwei Zhuang <siwei.zhuang@unsw.edu.au>

--- apps/Arm/vm_cross_connector/components/CrossvmInit/crossvm_init.c
+++ apps/Arm/vm_cross_connector/components/CrossvmInit/crossvm_init.c
@@ -7,16 +7,28 @@
 #include <string.h>
 #include <camkes.h>
 
+void vm0_event_handler(void *arg)
+{
+    printf("Got an event from vm0\n");
+	ready_reg_callback(vm0_event_handler, NULL);
+}
+
+void vm1_event_handler(void *arg)
+{
+    printf("Got an event from vm1\n");
+	ready1_reg_callback(vm1_event_handler, NULL);
+}
+
 int run(void)
 {
     memset(dest, '\0', 4096);
-    strcpy(dest, "This is a crossvm dataport test string\n");
+    strcpy(dest, "This is a crossvm dataport test string to VM0\n");
 
-    while (1) {
-        ready_wait();
-        printf("Got an event\n");
-        done_emit_underlying();
-    }
+    memset(dest1, '\0', 4096);
+    strcpy(dest1, "This is a crossvm dataport test string to VM1\n");
+
+	ready_reg_callback(vm1_event_handler, NULL);
+	ready1_reg_callback(vm0_event_handler, NULL);
 
     return 0;
 }
