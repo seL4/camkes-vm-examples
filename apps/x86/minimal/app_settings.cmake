793b435e2e0aed3a090b57d1f7aa7895ac0f3281
Chris Guikema <chris.guikema@dornerworks.com>
2023-10-27 10:50:02 -0400

    x86, minimal: compile 64-bit VM with HPET
    Mostly a dummy commit for HPET CI testing.
    
    Signed-off-by: Chris Guikema <chris.guikema@dornerworks.com>

--- apps/x86/minimal/app_settings.cmake
+++ apps/x86/minimal/app_settings.cmake
@@ -7,5 +7,7 @@
 cmake_minimum_required(VERSION 3.8.2)
 
 # Define kernel config options
-set(KernelSel4Arch ia32 CACHE STRING "" FORCE)
 set(KernelMaxNumNodes 1 CACHE STRING "" FORCE)
+set(LibSel4VMMUseHPET ON CACHE BOOL "" FORCE)
+set(KernelSel4Arch x86_64 CACHE STRING "" FORCE)
+set(KernelX86_64VTX64BitGuests ON CACHE BOOL "" FORCE)
