cfb4cb90648be474b5e70151cce5c51dd7451baf
Chris Guikema <chris.guikema@dornerworks.com>
2023-11-14 14:54:44 -0500

    x86: add minimal_64 application for 64-bit tests
    Signed-off-by: Chris Guikema <chris.guikema@dornerworks.com>

---
+++ apps/x86/minimal_64/app_settings.cmake
@@ -0,0 +1,12 @@
+#
+# Copyright 2023, DornerWorks
+#
+# SPDX-License-Identifier: BSD-2-Clause
+#
+
+cmake_minimum_required(VERSION 3.8.2)
+
+# Define kernel config options
+set(KernelSel4Arch x86_64 CACHE STRING "" FORCE)
+set(KernelX86_64VTX64BitGuests ON CACHE BOOL "" FORCE)
+set(KernelMaxNumNodes 1 CACHE STRING "" FORCE)
