ba5d70217b1c7d15091ed1630da250dc00945d50
test <srstingl@my.loyno.edu>
2024-06-06 16:28:07 -0500

    Updating vm multi

--- apps/Arm/vm_multi/scripts/run-vm-multi.sh
+++ apps/Arm/vm_multi/scripts/run-vm-multi.sh
@@ -4,7 +4,7 @@ if [ "$#" -eq 1 ]; then
     image=$1
 else
     # use default
-    image=$HOME/capsule-dev/camkes-vm-examples-manifest/build/vm_multi/images/capdl-loader-image-arm-qemu-arm-virt
+    image=$HOME/GitHub/capsule-dev/camkes-vm-examples-manifest/build/vm_multi/images/capdl-loader-image-arm-qemu-arm-virt
 fi
 
 if [ ! -f $image ]; then
@@ -17,7 +17,7 @@ qemu-system-aarch64 \
     -cpu cortex-a53 \
     -nographic \
     -smp 4 \
-    -m size=2048 \
+    -m size=3072 \
     -netdev tap,id=mynet0,ifname=tap0,script=no,downscript=no \
     -device e1000,netdev=mynet0,mac=52:55:00:d1:55:01 \
     -kernel $image
