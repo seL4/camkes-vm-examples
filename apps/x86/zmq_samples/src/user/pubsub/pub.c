5009e242b333bab5a4b374bcd8a3a6ea47c5dd92
Gerwin Klein <gerwin.klein@proofcraft.systems>
2026-06-12 09:05:20 +1000

    trivial: style fixes
    Signed-off-by: Gerwin Klein <gerwin.klein@proofcraft.systems>

--- apps/x86/zmq_samples/src/user/pubsub/pub.c
+++ apps/x86/zmq_samples/src/user/pubsub/pub.c
@@ -15,7 +15,7 @@
 #include <unistd.h>
 #define randof(num)  (int) ((float) (num) * random () / (RAND_MAX + 1.0))
 
-int main (int argc, char *argv[])
+int main(int argc, char *argv[])
 {
     const char *bind_to;
     int roundtrip_count;
@@ -27,60 +27,60 @@ int main (int argc, char *argv[])
     zmq_msg_t msg;
 
     if (argc != 4) {
-        printf ("usage: %s <bind-to> <message-size> "
-                "<roundtrip-count>\n", argv[0]);
+        printf("usage: %s <bind-to> <message-size> "
+               "<roundtrip-count>\n", argv[0]);
         return -1;
     }
     bind_to = argv[1];
-    message_size = atoi (argv[2]);
-    roundtrip_count = atoi (argv[3]);
+    message_size = atoi(argv[2]);
+    roundtrip_count = atoi(argv[3]);
 
-    ctx = zmq_init (1);
+    ctx = zmq_init(1);
     if (!ctx) {
-        printf ("error in zmq_init: %s\n", zmq_strerror (errno));
+        printf("error in zmq_init: %s\n", zmq_strerror(errno));
         return -1;
     }
 
-    s = zmq_socket (ctx, ZMQ_PUB);
+    s = zmq_socket(ctx, ZMQ_PUB);
     if (!s) {
-        printf ("error in zmq_socket: %s\n", zmq_strerror (errno));
+        printf("error in zmq_socket: %s\n", zmq_strerror(errno));
         return -1;
     }
-    rc = zmq_bind (s, bind_to);
+    rc = zmq_bind(s, bind_to);
     if (rc != 0) {
-        printf ("error in zmq_bind: %s\n", zmq_strerror (errno));
+        printf("error in zmq_bind: %s\n", zmq_strerror(errno));
         return -1;
     }
 
 
-    srandom ((unsigned) time (NULL));
+    srandom((unsigned) time(NULL));
     printf("%d\n", roundtrip_count);
-    for (int i=0; i < roundtrip_count; i++) {
+    for (int i = 0; i < roundtrip_count; i++) {
         //  Get values that will fool the boss
         //printf("Sending\n");
         int zipcode, temperature, relhumidity;
-        zipcode     = randof (100000);
-        temperature = randof (215) - 80;
-        relhumidity = randof (50) + 10;
+        zipcode     = randof(100000);
+        temperature = randof(215) - 80;
+        relhumidity = randof(50) + 10;
 
         //  Send message to all subscribers
         char update [20];
-        sprintf (update, "%05d %d %d %d", zipcode, temperature, relhumidity, i);
-        int len = zmq_send (s, update, strnlen(update, 20), 0);
+        sprintf(update, "%05d %d %d %d", zipcode, temperature, relhumidity, i);
+        int len = zmq_send(s, update, strnlen(update, 20), 0);
         //printf("%d\n", len);
         sleep(1);
     }
 
 
-    rc = zmq_close (s);
+    rc = zmq_close(s);
     if (rc != 0) {
-        printf ("error in zmq_close: %s\n", zmq_strerror (errno));
+        printf("error in zmq_close: %s\n", zmq_strerror(errno));
         return -1;
     }
 
-    rc = zmq_ctx_term (ctx);
+    rc = zmq_ctx_term(ctx);
     if (rc != 0) {
-        printf ("error in zmq_ctx_term: %s\n", zmq_strerror (errno));
+        printf("error in zmq_ctx_term: %s\n", zmq_strerror(errno));
         return -1;
     }
 
